297514d670d657535e52b6d588b2d16825f1dc38
lx kish <urban.ktulhu@gmail.com>
2020-06-20 17:59:31 +1200

    Updates

---
+++ precache-manifest.c027fddcd4c72bedd2f5a38dbb87a374.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "753b973e5a467c1202d4ab3ae12611cc",
+    "url": "/multiplay-react/index.html"
+  },
+  {
+    "revision": "df4ec28467d2f8b903a6",
+    "url": "/multiplay-react/static/css/main.6815ea75.chunk.css"
+  },
+  {
+    "revision": "d5de3c0c91e680be1bec",
+    "url": "/multiplay-react/static/js/2.5ba0a642.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/multiplay-react/static/js/2.5ba0a642.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df4ec28467d2f8b903a6",
+    "url": "/multiplay-react/static/js/main.97ef88ab.chunk.js"
+  },
+  {
+    "revision": "4b296a1d5225ce27ac3b",
+    "url": "/multiplay-react/static/js/runtime-main.32f8f433.js"
+  }
+]);
