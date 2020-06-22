fcb2e9d572f6f6ad300fbe617451d31402ad57d3
lx kish <urban.ktulhu@gmail.com>
2020-06-22 12:34:09 +1200

    Updates

--- precache-manifest.80f2d1e4f74543f2dab0105fd0930702.js
+++ precache-manifest.80f2d1e4f74543f2dab0105fd0930702.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "acefe888016e249e955af52694a6eb98",
+    "revision": "1e1a11a2917289961a9b723d8c5e71ef",
     "url": "/multiplay-react/index.html"
   },
   {
-    "revision": "9b5e6e8d9714f0783dfd",
+    "revision": "a94df40a455e94dd42d9",
     "url": "/multiplay-react/static/css/main.410b0c8c.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/multiplay-react/static/js/2.7cfe32cf.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "9b5e6e8d9714f0783dfd",
-    "url": "/multiplay-react/static/js/main.9ef3e733.chunk.js"
+    "revision": "a94df40a455e94dd42d9",
+    "url": "/multiplay-react/static/js/main.d17be6e5.chunk.js"
   },
   {
     "revision": "4b296a1d5225ce27ac3b",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/multiplay-react/precache-manifest.2f464c8b89f602a82e4ebbae1c885c70.js"
+  "/multiplay-react/precache-manifest.80f2d1e4f74543f2dab0105fd0930702.js"
 );
 
 self.addEventListener('message', (event) => {
