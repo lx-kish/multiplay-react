cdcb50fd8e7d754c3a88eeff977dc2f7117fa890
lx kish <urban.ktulhu@gmail.com>
2020-11-30 23:10:06 +1300

    fix(core): correct empty-stick line for stick header

--- src/components/tab-empty-line/tab-empty-line.component.jsx
+++ src/components/tab-empty-line/tab-empty-line.component.jsx
@@ -4,7 +4,8 @@ const EmptyLine = props => {
 
     return (
         <div className='tab__line--empty-stick'>
-            <div className='tab__heading-cell tab__heading-cell--side'>{props.sign}</div>
+            <div className='tab__heading-cell tab__heading-cell--side'>{" "}</div>
+            {/* <div className='tab__heading-cell tab__heading-cell--side'>{props.sign}</div> */}
         </div>
     )
 };
